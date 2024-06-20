41cb2524770b83e40273b3043dfa6d2cc7e86a45
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-06-20 21:48:01 +0200

    Make the example more interesting

---
+++ examples/src/main/java/com/example/Greeting.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Greeting {
+    public static String getGreeting() {
+        return "Hello, rbe_repos.bzl!";
+    }
+}
--- examples/src/main/java/com/example/Main.java
+++ examples/src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello remote_repos.bzl!");
+        System.out.println(Greeting.getGreeting());
     }
 }
